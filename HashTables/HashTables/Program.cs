2f9759c556d2bcf80d339702c0391f7c263e7d54
ravi04ez <ezekie.em@gmail.com>
2022-07-28 16:32:30 +0530

    wlcm Msg Hash Tables

---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HashTables
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t##############################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO HASH TABLES PROGRAM");
+            Console.WriteLine("\t\t\t\t##############################################");
+            Console.ReadKey();
+
+        }
+    }
+}
